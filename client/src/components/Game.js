16106f43644582bcbe7bbb1a53976fd03f9f70a5
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-02-17 22:11:33 -0500

    Linking 2 different users

---
+++ client/src/components/Game.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Game({channel}) {
+    const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2)
+
+    if (!playersJoined) {
+        return <div>"Waiting for other player"</div>
+    }
+  return (
+    <div>Game</div>
+  )
+}
+
+export default Game
