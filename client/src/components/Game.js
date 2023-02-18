4f480e43f88713f441a8f31c719641f863fc88ae
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-02-18 00:21:23 -0500

    Player Connection

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 function Game({channel}) {
     const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2)
 
+    channel.on("user.watching.start", (event) => {
+        setPlayersJoined(event.watcher_count = 2)
+    })
     if (!playersJoined) {
         return <div>"Waiting for other player"</div>
     }
