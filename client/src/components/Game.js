57f5dd8ec45260de8b6b4ccb3b54be2e2ea7f556
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-02-18 00:41:06 -0500

    CSS Added

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Board from './Board'
 
 function Game({channel}) {
     const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2)
@@ -10,7 +11,10 @@ function Game({channel}) {
         return <div>"Waiting for other player"</div>
     }
   return (
-    <div>Game</div>
+    <div className="gameContainer">
+        <Board />
+        
+    </div>
   )
 }
 
