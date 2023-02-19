bf890f342b012b3e9fe0e3dcce80e829f159afe7
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-02-18 20:18:43 -0500

    Winning Patterns Added

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useChannelStateContext, useChatContext} from 'stream-chat-react'
 import Square from './Square'
 
 function Board() {
@@ -6,18 +7,42 @@ function Board() {
     const [player, setPlayer] = useState("X")
     const [turn, setTurn] = useState("X")
 
-    const chooseSquare = (square) => {
+    const { channel } = useChannelStateContext()
+    const {client} = useChatContext()
+
+    const chooseSquare = async (square) => {
         if (turn === player && board[square] === "") {
             setTurn(player === "X" ? "O" : "X")
 
+            await channel.sendEvent({
+                type: "game-move",
+                data: {square, player},
+            })
             setBoard(board.map((val, idx) => {
                 if (idx === square && val === "") {
                     return player
                 }
                 return val
-            }))
+            })
+          )
         }
     }
+
+    channel.on((event) => {
+        if (event.type == "game-move" && event.user.id !== client.userID) {
+            const currentPlayer = event.data.player === "X" ? "O" : "X"
+            setPlayer(currentPlayer)
+            setTurn(currentPlayer)
+            setBoard(
+                board.map((val, idx) => {
+                    if (idx === event.data.square && val === "") {
+                        return event.data.player
+                    }
+                    return val
+                })
+            )
+        }
+    })
 
     return (
         <div className="board">
